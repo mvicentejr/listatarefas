24d28c7e93aced8ed918b8fc3de0bf34ab0d85a0
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2023-04-05 20:08:10 -0300

    Criando o primeiro componente

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+  },
+};
